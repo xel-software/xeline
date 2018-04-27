13e9c4c99947b33914a03aaffb82b3f76723bfb7
Xeline Development Team <incubator@mail.co.uk>
2018-04-27 21:48:52 +1000

    TSP fixes

--- callbacks/TSP_Example.js
+++ callbacks/TSP_Example.js
@@ -3,8 +3,8 @@
 var path = "Path: ";
 var i;
 for (i = 1; i < 49; i++) { 
-    path += storages[0][i] + "\t";
+    path += storages[0]["storage"][i] + "\t";
 }
 //path = path + storages[1];
-console.log("A miner has found a PATH less than 40,000 miles: " + storages[0]);
+console.log("A miner has found a PATH less than 40,000 miles: cost = " + storages[0]["storage"][0]);
 console.log(path);
